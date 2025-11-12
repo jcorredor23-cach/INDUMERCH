08f9031a6bda2de73bf8e4320119643b27c9ad87
Javier Ivan Rodriguez <jcorredor28@estudiantes.areandina.edu.co>
2025-11-12 00:42:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9825304265-b3a6d",
+  "appId": "1:639961916479:web:b4031f2cf756df21a5848a",
+  "apiKey": "AIzaSyDLM--ZSnUYRsUlBVO-rSaddTnBvEGbbSA",
+  "authDomain": "studio-9825304265-b3a6d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "639961916479"
+};
